485efaf338efaa5ca3e80324123aa7e7721c6dcd
huiseliming <huiseliming@qq.com>
2023-12-30 21:03:59 +0800

    Update openvpn.sh

--- Arch/openvpn.sh
+++ Arch/openvpn.sh
@@ -3,17 +3,16 @@ sudo systemctl enable NetworkManager.service
 sudo systemctl start NetworkManager.service
 
 sudo mkdir -p /etc/sysctl.d
-sudo vim /etc/sysctl.d/40-ipv6.conf
-# >> net.ipv6.conf.all.disable_ipv6=1
-# >> net.ipv6.conf.default.disable_ipv6=1
-# >> net.ipv6.conf.lo.disable_ipv6=1
-# >> net.ipv6.conf.eth0.disable_ipv6=1
-
+sudo sh -c "echo 'net.ipv6.conf.all.disable_ipv6=1' > /etc/sysctl.d/40-ipv6.conf"
+sudo sh -c "echo 'net.ipv6.conf.default.disable_ipv6=1' >> /etc/sysctl.d/40-ipv6.conf"
+for netname in `ls /sys/class/net/`
+do
+sudo sh -c "echo 'net.ipv6.conf.$netname.disable_ipv6=1' >> /etc/sysctl.d/40-ipv6.conf"
+done
 sudo systemctl restart systemd-sysctl.service
 sudo systemctl restart NetworkManager.service
 
-# nmcli connection import type openvpn file profile-56.ovpn
-nmcli connection import type openvpn file ?.ovpn
+nmcli connection import type openvpn file ~/client.ovpn
 
 sudo systemctl restart systemd-sysctl.service
 sudo systemctl restart NetworkManager.service
