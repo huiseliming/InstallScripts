163997675b74131504f77a525f23f6b466a40662
huiseliming <huiseliming@qq.com>
2023-12-30 21:48:48 +0800

    Update openvpn.sh

--- Arch/openvpn.sh
+++ Arch/openvpn.sh
@@ -13,7 +13,7 @@ sudo systemctl restart systemd-sysctl.service
 sudo systemctl restart NetworkManager.service
 
 # sudo scp profile-56.ovpn huiseliming@192.168.0.104:~/client.ovpn
-nmcli connection import type openvpn file ~/client.ovpn
+sudo nmcli connection import type openvpn file ~/client.ovpn
 
 sudo systemctl restart systemd-sysctl.service
 sudo systemctl restart NetworkManager.service
