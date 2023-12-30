38d45a5e28f6be5bf8a905c463bc4de057c160fa
huiseliming <huiseliming@qq.com>
2023-12-31 00:00:24 +0800

    Update openvpn.sh

--- Arch/openvpn.sh
+++ Arch/openvpn.sh
@@ -14,12 +14,12 @@ sudo systemctl restart NetworkManager.service
 
 # sudo scp profile-56.ovpn huiseliming@192.168.0.104:~/client.ovpn
 sudo nmcli connection import type openvpn file ~/client.ovpn
-sudo sh -c "echo '[vpn]' > /etc/NetworkManager/system-connections/client.nmconnection"
-sudo sh -c "echo 'connection-type=password' > /etc/NetworkManager/system-connections/client.nmconnection"
-sudo sh -c "echo 'password-flags=0' > /etc/NetworkManager/system-connections/client.nmconnection"
-sudo sh -c "echo 'username=$1' > /etc/NetworkManager/system-connections/client.nmconnection"
-sudo sh -c "echo '[vpn-secrets]' > /etc/NetworkManager/system-connections/client.nmconnection"
-sudo sh -c "echo 'password=$2' > /etc/NetworkManager/system-connections/client.nmconnection"
+sudo sh -c "echo '[vpn]' >> /etc/NetworkManager/system-connections/client.nmconnection"
+sudo sh -c "echo 'connection-type=password' >> /etc/NetworkManager/system-connections/client.nmconnection"
+sudo sh -c "echo 'password-flags=0' >> /etc/NetworkManager/system-connections/client.nmconnection"
+sudo sh -c "echo 'username=$1' >> /etc/NetworkManager/system-connections/client.nmconnection"
+sudo sh -c "echo '[vpn-secrets]' >> /etc/NetworkManager/system-connections/client.nmconnection"
+sudo sh -c "echo 'password=$2' >> /etc/NetworkManager/system-connections/client.nmconnection"
  
 sudo nmcli connection show
 
