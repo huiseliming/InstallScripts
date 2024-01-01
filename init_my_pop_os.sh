c95e6d3f869f3339f1739d536055371fe35ab094
huiseliming <huiseliming@qq.com>
2024-01-01 17:43:28 +0800

    WIP

---
+++ init_my_pop_os.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -v
+
+sudo apt install git
+git config --global user.name "huiseliming"
+git config --global user.email "huiseliming@qq.com"
+
+sudo apt install snapd
+snap install code --classic
+snap install chromium
+
+
+
+
